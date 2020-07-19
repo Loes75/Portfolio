df2e1c5fd485f3e0a0139c2379e002c1cc6a4aa4
LauraOchoa <lauraochoae7@gmail.com>
2020-07-18 23:44:52 -0500

    Updates

---
+++ precache-manifest.480c93e051dffc742fad1bb26a9aaecc.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805ea7bccd4a153c12659c9841ff26ef",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "d79d8aa3ddd4caf1791a",
+    "url": "/Portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "865c6aa922f0e9764ad6",
+    "url": "/Portfolio/static/css/main.6e57b445.chunk.css"
+  },
+  {
+    "revision": "d79d8aa3ddd4caf1791a",
+    "url": "/Portfolio/static/js/2.2b20be3b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.2b20be3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "865c6aa922f0e9764ad6",
+    "url": "/Portfolio/static/js/main.db5919e1.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "2719760172113d02d4f90892f5ef4ed3",
+    "url": "/Portfolio/static/media/Bg2.27197601.png"
+  },
+  {
+    "revision": "11a6a67c2066dd25e587f8aa51ad30ef",
+    "url": "/Portfolio/static/media/HV_LauraOchoa.11a6a67c.pdf"
+  },
+  {
+    "revision": "32db3ee372cab127a218d13d7a94a30a",
+    "url": "/Portfolio/static/media/PhoneHotel.32db3ee3.png"
+  },
+  {
+    "revision": "8c8d32ddeae48f78fbd52513e2eaed5b",
+    "url": "/Portfolio/static/media/PhoneImg.8c8d32dd.png"
+  },
+  {
+    "revision": "45b82b67b0c94ce9727a8790cb0dfc0d",
+    "url": "/Portfolio/static/media/PhoneNasa.45b82b67.png"
+  },
+  {
+    "revision": "2639ee25758fda2201f878cefa033637",
+    "url": "/Portfolio/static/media/PhoneRestaurant.2639ee25.png"
+  },
+  {
+    "revision": "9d9ef2f89197a58f06667da2b82f2ae2",
+    "url": "/Portfolio/static/media/TabletCovid.9d9ef2f8.png"
+  },
+  {
+    "revision": "144a73c195aa611fce851df4175b7e0d",
+    "url": "/Portfolio/static/media/TabletHotel.144a73c1.png"
+  },
+  {
+    "revision": "ecc368506a33a9632225b35717f174e3",
+    "url": "/Portfolio/static/media/TabletImg.ecc36850.png"
+  },
+  {
+    "revision": "018aa6208cc45ea07e07ae185fba4082",
+    "url": "/Portfolio/static/media/TabletNasa.018aa620.png"
+  },
+  {
+    "revision": "f952f0552977927ebb03ba646d7afb40",
+    "url": "/Portfolio/static/media/TabletRestaurant.f952f055.png"
+  },
+  {
+    "revision": "53bf35309af9502b55c911f1434f6a0e",
+    "url": "/Portfolio/static/media/covid.53bf3530.jpg"
+  },
+  {
+    "revision": "79739363083388ba5863cf64550e15db",
+    "url": "/Portfolio/static/media/digitization.79739363.png"
+  },
+  {
+    "revision": "cff4e4e98a7ec18057cba331ef7d938d",
+    "url": "/Portfolio/static/media/hotel2.cff4e4e9.JPG"
+  },
+  {
+    "revision": "dad1fd422afa2dad6a54892269b6ffd5",
+    "url": "/Portfolio/static/media/imageFinder.dad1fd42.JPG"
+  },
+  {
+    "revision": "ffa7a14a78963de7603fe4594ad4fc9e",
+    "url": "/Portfolio/static/media/nasa.ffa7a14a.JPG"
+  },
+  {
+    "revision": "e41bde064363a2f5a4bcd86b3c55fd94",
+    "url": "/Portfolio/static/media/restaurant.e41bde06.JPG"
+  }
+]);
